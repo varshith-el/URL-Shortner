f8941a9a7d796dc710cac8f7dd5abc55394617a9
varshith-el <147407724+varshith-el@users.noreply.github.com>
2023-10-13 14:10:29 +0530

    Update settings.py, removed the key

--- URL_Shortner/settings.py
+++ URL_Shortner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6+--gw-x+2=5&j6bk@e7s#wami3q#qggf(n40z*u+dn7u1o)b_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
